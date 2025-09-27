46687edde2e1f3158c05fa7bd97c2b6121f04b72
646887542 <hongminecraft@qq.com>
2025-09-27 09:01:03 +0800

    重构满天星

---
+++ src/pi/classes/blocks/ItemDefenseTurret.java
@@ -0,0 +1,27 @@
+package pi.classes.blocks;
+
+import mindustry.gen.Bullet;
+import mindustry.gen.Groups;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+
+public class ItemDefenseTurret extends ItemTurret {
+
+    public ItemDefenseTurret(String name) {
+        super(name);
+        buildType = ItemDefenseBuild::new;
+    }
+
+    public class ItemDefenseBuild extends ItemTurret.ItemTurretBuild {
+
+        @Override
+        public void updateTile() {
+            this.target = Groups.bullet.intersect(this.x - ItemDefenseTurret.this.range, this.y - ItemDefenseTurret.this.range, ItemDefenseTurret.this.range * 2.0F, ItemDefenseTurret.this.range * 2.0F).min((b) -> b.team != this.team && b.type().hittable, (b) -> b.dst2(this));
+
+            if (this.target != null && !this.target.isAdded()) {
+                this.target = null;
+            }
+
+            super.updateTile();
+        }
+    }
+}
--- src/pi/content/PiTechTree.java
+++ src/pi/content/PiTechTree.java
@@ -1,8 +1,12 @@
 package pi.content;
 
+import arc.struct.Seq;
+import mindustry.game.Objectives;
+
 import static mindustry.content.TechTree.*;
 import static mindustry.content.Blocks.*;
 import static pi.content.PiBlockTypes.*;
+import static pi.content.PiSectors.*;
 
 public class PiTechTree {
     public static void Load() {
@@ -11,6 +15,10 @@ public class PiTechTree {
             node(BaseCore, ()->{});
             //LaunchPad
             node(InterstellarLaunchPad, ()->{});
+            //turret
+            node(Gypsophila, Seq.with(new Objectives.SectorComplete(BaseArea), new Objectives.Research(BaseCore)), ()->{});
+            //sector
+            node(BaseArea, ()->{});
         });
     }
 }
