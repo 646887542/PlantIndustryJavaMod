3f335638e4dfe883016c889764aa0e0ca6dbd130
646887542 <hongminecraft@qq.com>
2025-10-02 20:18:00 +0800

    优化Gypsophila

--- src/pi/classes/blocks/ItemDefenseTurret.java
+++ src/pi/classes/blocks/ItemDefenseTurret.java
@@ -14,9 +14,7 @@ public class ItemDefenseTurret extends ItemTurret {
 
         @Override
         public void updateTile() {
-            if (Groups.bullet.intersect(this.x - ItemDefenseTurret.this.range, this.y - ItemDefenseTurret.this.range, ItemDefenseTurret.this.range * 2.0F, ItemDefenseTurret.this.range * 2.0F).min((b) -> b.team != this.team && b.type().hittable, (b) -> b.dst2(this)) != null) {
-                this.target = Groups.bullet.intersect(this.x - ItemDefenseTurret.this.range, this.y - ItemDefenseTurret.this.range, ItemDefenseTurret.this.range * 2.0F, ItemDefenseTurret.this.range * 2.0F).min((b) -> b.team != this.team && b.type().hittable, (b) -> b.dst2(this));
-            }
+            this.target = Groups.bullet.intersect(this.x - ItemDefenseTurret.this.range, this.y - ItemDefenseTurret.this.range, ItemDefenseTurret.this.range * 2.0F, ItemDefenseTurret.this.range * 2.0F).min((b) -> b.team != this.team && b.type().hittable, (b) -> b.dst2(this));
 
             if (this.target != null && !this.target.isAdded()) {
                 this.target = null;
